fd1822d2781f6bc8a97133e8b9883f8c26fe6fd6
신은찬 <dmscks3126@gmail.com>
2025-07-25 13:08:13 +0900

    250725 코딩웨일 과외

--- coding_whale/250708_cafe_order_pjt/cafe_order_pjt/src/star.java
+++ coding_whale/250708_cafe_order_pjt/cafe_order_pjt/src/star.java
@@ -1,29 +1,87 @@
+import java.util.Scanner;
+
 public class star {
-  public static void main(String[] args) {
-    // 직각 삼각형 별찍기
-//    for (int i=0; i<5; i++) {
-//      for (int j=0; j<=i; j++) {
-//        System.out.print("*");
-//      }
-//      System.out.println();
-//    }
+    public static void main(String[] args) {
+//         직각 삼각형 별찍기
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("직각 삼각형의 길이를 입력하세요 : ");
+//        int cm = sc.nextInt();
+//
+//        for (int i = 0; i < cm; i++) {
+//            for (int j = 0; j <= i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
 
 
-    // 마름모 별찍기
-    for (int i=0; i<5; i++) {
-      if (i == 0 || i == 4) {
-        System.out.println("  *  ");
-      } else if (i == 1 || i == 3) {
-        System.out.println(" *** ");
-      } else {
-        System.out.println("*****");
-      }
+        // 마름모 별찍기 // 가로 , 세로로 바꾸기
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("마름모의 길이를 입력하세요 (홀수, 최소 3이상) : ");
+//        int 세로 = sc.nextInt();
+//        int 가로 = sc.nextInt();
+//
+//        for (int i = 1; i <= 세로; i++) {
+//            if (세로 % i == 0) {
+//                System.out.println("  *  ");
+//            } else if (세로 % i == 1) {
+//                System.out.println(" *** ");
+//            } else if (세로 % i == 세로 / 2) {
+//                for (int j = 1; j <= 가로; j++) {
+//                    System.out.print("*");
+//                }
+//                System.out.println();
+//            }
+//        }
+
+        // 가로 3, 세로 3
+//        System.out.println("  *  ");
+//        System.out.println("* * *");
+//        System.out.println("  *  ");
+//
+//        // 가로(별의 개수) 5, 세로 5
+//        // 추가된 가로만큼 첫 줄에 공백이 생김 > 영향을 줌 > 변수에 반영
+//        System.out.println("    *    ");
+//        System.out.println("  * * *  ");
+//        System.out.println("* * * * *");
+//        System.out.println("  * * *  ");
+//        System.out.println("    *    ");
+
+
+
+
+
+
+//        for (int i = 0; i < 5; i++) {
+//            if (i == 0 || i == 4) {
+//                System.out.println("  *  ");
+//            } else if (i == 1 || i == 3) {
+//                System.out.println(" *** ");
+//            } else {
+//                System.out.println("*****");
+//            }
+//        }
+
+        // 원 별찍기
+        System.out.println("      ***      ");
+        System.out.println("    *******      ");
+        System.out.println("  ***********      ");
+        System.out.println("***************      ");
+        System.out.println("***************      ");
+        System.out.println("  ***********      ");
+        System.out.println("    *******      ");
+        System.out.println("      ***      ");
+//
+//
+//
+//
+//        // 별 별찍기 (오각형)
+//        System.out.println("      *      ");
+//        System.out.println("     * *     ");
+//        System.out.println("  **     **      ");
+//        System.out.println("    * * *      ");
+//        System.out.println("   *     *      ");
+
+
     }
-
-//    System.out.println("  *  ");
-//    System.out.println(" *** ");
-//    System.out.println("*****");
-//    System.out.println(" *** ");
-//    System.out.println("  *  ");
-  }
 }
