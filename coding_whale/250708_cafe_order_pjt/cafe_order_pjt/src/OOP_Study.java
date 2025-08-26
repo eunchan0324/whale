535109d95357593e6faf8b095bd4e20623d94d06
신은찬 <dmscks3126@gmail.com>
2025-08-26 22:40:15 +0900

    250826 study

--- coding_whale/250708_cafe_order_pjt/cafe_order_pjt/src/OOP_Study.java
+++ coding_whale/250708_cafe_order_pjt/cafe_order_pjt/src/OOP_Study.java
@@ -1,20 +1,44 @@
+interface  Predator {
+  String getFood();
+
+  default void printFood() {
+    System.out.printf("my food is %s\n", getFood());
+  }
+}
+
 class Animal {
   String name;
 
-  public void setName (String name) {
+  void setName(String name) {
     this.name = name;
+  }
+}
+
+
+class Tiger extends Animal implements Predator {
+  public String getFood() {
+    return "apple";
+  }
+}
+
+class Lion extends Animal implements Predator{
+  public String getFood() {
+    return "banana";
+  }
+}
+
+class ZooKeeper extends Animal {
+  void feed(Predator predator) { // 호랑이가 오면 사과를 던져준다.
+    System.out.println("feed " + predator.getFood());
   }
 }
 
 public class OOP_Study {
   public static void main(String[] args) {
-    Animal cat = new Animal();
-    cat.setName("boby");
-
-    Animal dog = new Animal();
-    dog.setName("happy");
-
-    System.out.println(cat.name);
-    System.out.println(dog.name);
+    ZooKeeper zooKeeper = new ZooKeeper();
+    Tiger tiger = new Tiger();
+    Lion lion = new Lion();
+    zooKeeper.feed(tiger);
+    zooKeeper.feed(lion);
   }
 }
