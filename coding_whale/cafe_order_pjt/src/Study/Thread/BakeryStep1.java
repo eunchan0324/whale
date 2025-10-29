b87d44add8adcab88c92ce930aaa6d40112b420a
신은찬 <dmscks3126@gmail.com>
2025-10-29 15:33:24 +0900

    Study: Thread practice 1

---
+++ coding_whale/cafe_order_pjt/src/Study/Thread/BakeryStep1.java
@@ -0,0 +1,43 @@
+package Study.Thread;
+
+public class BakeryStep1 {
+    public static void main(String[] args) {
+        // 직원 1 : 반죽 담당
+        Thread baker = new Thread(() -> {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println("반죽 중..." + i + "번째 빵");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+            }
+        });
+
+        // 직원 2 : 포장 담당
+        Thread packer = new Thread(() -> {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println("포장 중 ..." + i + "번째 빵");
+                try {
+                    Thread.sleep(700);
+                } catch (InterruptedException e) {
+                }
+            }
+        });
+
+        // 두 직원 동시에 시작
+        baker.start();
+        packer.start();
+
+        // 메인 스레드가 직원들이 끝날 때까지 기다림
+        try {
+            baker.join();
+            packer.join();
+        } catch (InterruptedException e) {
+
+        }
+
+        System.out.println("모든 빵 완성!");
+
+    }
+}
+
