948d91d4be96556790fc0d73f9da3d4a508ab061
신은찬 <dmscks3126@gmail.com>
2025-10-30 12:51:59 +0900

    Study: Thread practice 3 : Synchronized

---
+++ coding_whale/cafe_order_pjt/src/Study/Thread/BakeryStep3_Synchronized.java
@@ -0,0 +1,30 @@
+package Study.Thread;
+
+public class BakeryStep3_Synchronized {
+    static int dough = 0; // 공유 반죽통
+
+    // 반죽을 추가하는 동기화된 메서드
+    public static synchronized void addDough() {
+        dough++; // 이 블록은 한 번에 한 스레드만 실행 가능
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable worker = () -> {
+            for (int i = 0; i < 10_000; i++) {
+                addDough(); // 안전하게 증가
+            }
+        };
+
+        Thread workerA = new Thread(worker);
+        Thread workerB = new Thread(worker);
+
+        workerA.start();
+        workerB.start();
+
+        workerA.join();
+        workerB.join();
+
+        System.out.println("기대 반죽량 : 20000");
+        System.out.println("실제 반죽량 : " + dough);
+    }
+}
