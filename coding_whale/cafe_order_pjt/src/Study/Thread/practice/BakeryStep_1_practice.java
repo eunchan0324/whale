9947ea190e3603436a18b23307283c76593c85cd
신은찬 <dmscks3126@gmail.com>
2025-11-02 22:52:27 +0900

    Study : review Thread 1

---
+++ coding_whale/cafe_order_pjt/src/Study/Thread/practice/BakeryStep_1_practice.java
@@ -0,0 +1,37 @@
+package Study.Thread.practice;
+
+public class BakeryStep_1_practice {
+    public static void main(String[] args) throws InterruptedException {
+        Runnable runnable = () -> {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println("반죽중..." + i + "번째 빵");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+
+        Thread baker = new Thread(runnable);
+
+        Thread packer = new Thread(() -> {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println("포장중..." + i + "번째 빵");
+                try {
+                    Thread.sleep(700);
+                } catch (InterruptedException e) {
+                }
+            }
+        });
+
+        baker.start();
+        System.out.println("베이커 스레드 시작");
+        packer.start();
+        System.out.println("페커 스레드 시작");
+
+        baker.join();
+        packer.join();
+
+        System.out.println("모든 빵 완성");
+    }
+}
