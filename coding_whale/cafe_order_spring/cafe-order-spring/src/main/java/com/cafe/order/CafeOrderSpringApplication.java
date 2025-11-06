9645a1c7a58a1a1a28ab1ba1733fdd5234b456d6
신은찬 <dmscks3126@gmail.com>
2025-11-06 18:47:12 +0900

    init : 카페 주문 프로그램 Spring Boot 프로젝트 생성

---
+++ coding_whale/cafe_order_spring/cafe-order-spring/src/main/java/com/cafe/order/CafeOrderSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cafe.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeOrderSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeOrderSpringApplication.class, args);
+    }
+
+}
